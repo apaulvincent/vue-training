5864dba2b0859cb49f0c71d594548bef99e21adb
apaulvincent <paulvincentdeocampo@gmail.com>
2017-06-16 09:20:15 +0800

    add files

---
+++ src/components/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'About Route'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    body {
+        color: red;
+
+        p {
+            color: blue
+        }
+    }
+</style>
